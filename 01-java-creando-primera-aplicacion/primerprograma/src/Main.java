f13f4070d7cb1e1f5f80d95882ae9ddd35e69f69
tahayk3 <tahayk3@gmail.com>
2025-04-29 17:37:05 -0600

    primer proyecto consola, primer curso

--- 01-java-creando-primera-aplicacion/primerprograma/src/Main.java
+++ 01-java-creando-primera-aplicacion/primerprograma/src/Main.java
@@ -2,7 +2,36 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula =8.2;
+
+        String sinopsis = """
+                Aqui se puede
+                poner varias lineas 
+                en una soloa
+                """;
+
+        System.out.println(sinopsis);
+
+
+        //comparar texto con equals
+        String contrasena= "12345";
+        if (contrasena.equals("12345")) {
+            System.out.println("Acceso autorizado!");
+        } else {
+            System.out.println("Contraseña incorrecta.");
+        }
+
+
+        //formatear datos
+        String nombre = "Maria";
+        int edad = 30;
+        double valor = 55.9999;
+        System.out.println(String.format("Mi nombre es %s, tengo %d años y hoy gasté %.2f dolares", nombre, edad, valor));
 
     }
 }
