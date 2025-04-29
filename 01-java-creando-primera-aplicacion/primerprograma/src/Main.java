0292f823da3c3368e5a3eb9d7eb1e5d8de84f46d
tahayk3 <tahayk3@gmail.com>
2025-04-29 11:56:56 -0600

    cambiando mensaje de primera clase

--- 01-java-creando-primera-aplicacion/primerprograma/src/Main.java
+++ 01-java-creando-primera-aplicacion/primerprograma/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiéndome en Java!");
 
     }
 }
