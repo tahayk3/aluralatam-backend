ad893e13808cfca32ac60d74cdfdb39c11db11a6
tahayk3 <tahayk3@gmail.com>
2025-05-02 12:25:25 -0600

    aplicando polimorfismo

--- 02-java-aplicando-orientacion-objetos/screenmatch/src/Principal.java
+++ 02-java-aplicando-orientacion-objetos/screenmatch/src/Principal.java
@@ -1,19 +1,35 @@
 import com.tahayk3.screenmatch.modelos.Pelicula;
+import com.tahayk3.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
 
+        //creando e insertando datos de pelicula
         Pelicula miPelicula = new Pelicula();
         miPelicula.setNombre("La familia del futuro");
         miPelicula.setFechaDeLanzamiento(2007);
         miPelicula.setDuracionEnMinutos(95);
         miPelicula.setDirector("pablo cuarom");
         miPelicula.setIncluidoEnElPlan(true);
-
-        miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
         miPelicula.evalua(7.8);
+
+        //mostrar datos de pelicula
+        miPelicula.muestraFichaTecnica();
         System.out.println("Média de evaluaciones de la película: " +miPelicula.calculaMedia());
+
+        //creando e insertando datos de serie
+        Serie theLastOfUs = new Serie();
+        theLastOfUs.setNombre("The last of us");
+        theLastOfUs.setFechaDeLanzamiento(2022);
+        theLastOfUs.setTemporadas(2);
+        theLastOfUs.setMinutosPorEpisodio(55);
+        theLastOfUs.setEpisodiosPorTemporada(10);
+
+        //mostrar datos de serie
+        theLastOfUs.muestraFichaTecnica();
+        System.out.println(theLastOfUs.getDuracionEnMinutos());
+
     }
 }
--- 02-java-aplicando-orientacion-objetos/screenmatch/src/com/tahayk3/screenmatch/modelos/Serie.java
+++ 02-java-aplicando-orientacion-objetos/screenmatch/src/com/tahayk3/screenmatch/modelos/Serie.java
@@ -5,6 +5,11 @@ public class Serie extends Titulo {
     private int episodiosPorTemporada;
     private int minutosPorEpisodio;
 
+    @Override
+    public int getDuracionEnMinutos() {
+        return temporadas * episodiosPorTemporada * minutosPorEpisodio;
+    }
+
     public int getTemporadas() {
         return temporadas;
     }
@@ -12,7 +17,7 @@ public class Serie extends Titulo {
     public void setTemporadas(int temporadas) {
         this.temporadas = temporadas;
     }
-
+    
     public int getEpisodiosPorTemporada() {
         return episodiosPorTemporada;
     }
