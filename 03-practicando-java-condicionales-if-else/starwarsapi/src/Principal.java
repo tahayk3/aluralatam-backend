178ed281cad1dcccf831fc2ec063d51e77b2d84f
tahayk3 <tahayk3@gmail.com>
2025-05-26 10:56:07 -0600

    terminando poo java

---
+++ 03-practicando-java-condicionales-if-else/starwarsapi/src/Principal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner lectura = new Scanner(System.in);
+        ConsultaPelicula consulta = new ConsultaPelicula();
+        System.out.println("Escriba el numero de la pelicula de Star Wars que quiere consultar:");
+        try{
+            var numeroDePelicula = Integer.valueOf(lectura.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+            GeneradorDeArchivo generador = new GeneradorDeArchivo();
+            generador.guardarJson(pelicula);
+        } catch (NumberFormatException e){
+            System.out.println("Número no encontrado "+e.getMessage());
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación.");
+        }
+    }
+}
