cef7e94905d38fe381c5f3409da3ae04bb98b89d
tahayk3 <tahayk3@gmail.com>
2025-07-08 10:51:59 -0600

    agregand  Lombok en clases

--- 09-spring-boot-3-dessarrola-api-rest-java/src/main/java/med/voll/api/direccion/Direccion.java
+++ 09-spring-boot-3-dessarrola-api-rest-java/src/main/java/med/voll/api/direccion/Direccion.java
@@ -1,5 +1,15 @@
 package med.voll.api.direccion;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Embeddable
 public class Direccion {
     private String calle;
     private String numero;
--- 09-spring-boot-3-dessarrola-api-rest-java/src/main/java/med/voll/api/medico/Medico.java
+++ 09-spring-boot-3-dessarrola-api-rest-java/src/main/java/med/voll/api/medico/Medico.java
@@ -1,14 +1,64 @@
 package med.voll.api.medico;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.direccion.DatosDireccion;
 import med.voll.api.direccion.Direccion;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 
+@Table(name = "medicos")
+@Entity(name = "Medico")
 public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String email;
     private String documento;
+
+    @Enumerated(EnumType.STRING)
     private Especialidad especialidad;
+
+    @Embedded
     private Direccion direccion;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
