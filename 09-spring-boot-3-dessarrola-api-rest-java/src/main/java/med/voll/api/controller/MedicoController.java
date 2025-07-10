5cd1f0686c3d1846c3edd339afcd41779c2c5964
tahayk3 <tahayk3@gmail.com>
2025-07-10 09:49:37 -0600

    ordenacion y cambios de paginacion por defecto

--- 09-spring-boot-3-dessarrola-api-rest-java/src/main/java/med/voll/api/controller/MedicoController.java
+++ 09-spring-boot-3-dessarrola-api-rest-java/src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DatosListaMedico> listar(Pageable pagination){
+    public Page<DatosListaMedico> listar(@PageableDefault(size = 10, sort = {"nombre"} ) Pageable pagination){
         return repository.findAll(pagination).map(DatosListaMedico::new);
     }
 }
