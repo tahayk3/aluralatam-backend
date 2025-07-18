291e8652ac7c7f12daa240369a2368df92d65b64
tahayk3 <tahayk3@gmail.com>
2025-07-18 13:44:24 -0600

    agregando funcionalidad de elegir Medico Aleatorio Disponible En La Fecha

---
+++ 11-spring-boot-3-documentar-probar-preparar-api-implementacion/src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -0,0 +1,48 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservaDeConsultas {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void reservar(DatosReservaConsulta datos){
+
+        if(!pacienteRepository.existsById(datos.idPaciente())){
+            throw new ValidacionException("No existe un paciente con el id informado");
+        }
+
+        if(datos.idMedico() != null && !medicoRepository.existsById(datos.idMedico())){
+            throw new ValidacionException("No existe un médico con el id informado");
+        }
+
+        var medico = elegirMedico(datos);
+        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, datos.fecha());
+        consultaRepository.save(consulta);
+    }
+
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+        if(datos.idMedico() != null){
+            return medicoRepository.getReferenceById(datos.idMedico());
+        }
+        if(datos.especialidad() == null){
+            throw new ValidacionException("Es necesario elegir una especialidad cuando no se elige un médico");
+        }
+
+        return medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(datos.especialidad(), datos.fecha());
+    }
+}
