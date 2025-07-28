1042e760a712ebb268aa5ef442cfda92ae737869
tahayk3 <tahayk3@gmail.com>
2025-07-28 11:47:20 -0600

    integrando open ai

---
+++ 13-gpt-java-integra-aplicacion-openai/src/main/java/com/tahayk3/ecomart/Controller/GeneradorDeProductosController.java
@@ -0,0 +1,62 @@
+package com.tahayk3.ecomart.Controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/generador")
+public class GeneradorDeProductosController {
+
+    private final ChatClient chatClient;
+
+    public GeneradorDeProductosController(ChatClient.Builder chatClientBuilder) {
+
+        this.chatClient = chatClientBuilder.build();
+
+    }
+
+    @GetMapping
+    public String generarProductos() {
+
+        var pregunta = "Genera 5 productos ecológicos";
+
+        return this.chatClient.prompt()
+
+                .user(pregunta)
+
+                .call()
+
+                .content();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
